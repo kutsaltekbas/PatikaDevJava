0068c3f28eff802af129adf85bfe309bc28fefa4
kutsal tekbas <kutsal_tekbass@hotmail.com>
2022-07-03 18:06:20 +0300

    triangle drawer project completed

---
+++ HomeWorkProjects/src/triangle/triangleDrawer.java
@@ -0,0 +1,23 @@
+package triangle;
+
+import java.util.Scanner;
+
+public class triangleDrawer {
+    public static void main(String[] args) {
+        int i,j,k,num;//Değişekenlerimizi tanımladık.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("pls enter a number");
+        num = scanner.nextInt();
+
+        for (i=1; i<num; i++) {
+            for(j=i; j<num;j++) {
+                System.out.print(" ");
+            }
+            for (k=1; k<=i; k++) {
+                System.out.print("*");
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
